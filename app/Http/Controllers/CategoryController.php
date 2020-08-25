c07bb58a7d4bb9a9fee266cad0779da91c04dcec
niloy009 <niloy.roy@northsouth.edu>
2020-08-25 15:31:05 +0600

    upto category

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Category;
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $categories = Category::orderBy('id', 'desc')->get();
+        return view('admin.category.index', compact('categories'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('admin.category.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //validation
+        $this->validate($request, ['name' => 'required']);
+
+        Category::create($request->all());
+        return redirect('/categories')->with('status',"Created Successfully");
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param \App\Category $category
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Category $category)
+    {
+        return view('admin.category.show',compact('category'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param \App\Category $category
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Category $category)
+    {
+        return view('admin.category.edit',compact('category'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param \App\Category $category
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Category $category)
+    {
+        //validation
+        $this->validate($request, ['name' => 'required']);
+
+        $category->update($request->all());
+        return redirect('/categories')->with('status',"Updated Successfully");
+
+
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param \App\Category $category
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Category $category)
+    {
+        $category->delete();
+        return redirect('/categories')->with('status',"Deleted Successfully");
+
+    }
+
+    public function statusUpdate(Category $category)
+    {
+        $category->update([
+            'status' => !$category->status
+        ]);
+        return redirect('/categories')->with('status',"Status Updated Successfully");
+
+    }
+}
---
+++ database/seeds/DatabaseSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Category;
+use App\User;
+use Illuminate\Support\Facades\DB;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+//         $this->call(UsersTableSeeder::class);
+        DB::statement('SET FOREIGN_KEY_CHECKS = 0');
+
+        User::truncate();
+        Category::truncate();
+
+        factory(User::class)->create([
+            'name' => 'Admin',
+            'email' => 'admin@mail.com',
+        ]);
+
+        factory(User::class)->create([
+            'name' => 'User',
+            'email' => 'user@mail.com',
+        ]);
+
+        factory(Category::class, 5)->create();
+    }
+}
